a3aed05ca3e23b69398b8388571e8d0cdbefcc8f
Danilo Mariano <ssadanilo@hotmail.com>
2024-01-31 17:44:10 -0300

    deixandotudopronto

---
+++ DeixandoTudoPronto.py
@@ -0,0 +1,8 @@
+pizza = "calabreza"
+print(f"A melhor pizza é a de {pizza}")
+
+ale01 = "Weiss"
+ale02 = "Hope Weiss"
+ale03 = "Weizen"
+
+print(f"As melhores cervejas são as de trigo, como: {ale01}, {ale02} e {ale03}")
