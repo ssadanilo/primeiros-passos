5ee59eef392af1b905400b9fdd1a1b56773b4943
Dev_Danilo <ssadanilo@hotmail.com>
2024-02-01 17:31:58 -0300

    DeixandoTudoPronto.py

--- DeixandoTudoPronto.py
+++ DeixandoTudoPronto.py
@@ -1,8 +1,8 @@
-pizza = "calabreza"
-print(f"A melhor pizza é a de {pizza}")
+pizza = 'calabreza'
+print(f'A melhor pizza é a de {pizza}')
 
-ale01 = "Weiss"
-ale02 = "Hope Weiss"
-ale03 = "Weizen"
+ale01 = 'Weiss'
+ale02 = 'Hope Weiss'
+ale03 = 'Weizen'
 
-print(f"As melhores cervejas são as de trigo, como: {ale01}, {ale02} e {ale03}")
+print(f'As melhores cervejas são as de trigo, como: {ale01}, {ale02} e {ale03}')
